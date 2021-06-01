8fb12d10355709083304482df63c0231df791ec4
Anja <anja.ishmukhametova@energi.team>
2021-06-01 17:21:54 +1000

    gnark test

---
+++ cubic_test.go
@@ -0,0 +1,36 @@
+package cubic
+
+import (
+	"testing"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend"
+	"github.com/consensys/gnark/backend/groth16"
+	"github.com/consensys/gnark/frontend"
+)
+
+func TestCubicEquation(t *testing.T) {
+	assert := groth16.NewAssert(t)
+
+	var cubicCircuit Circuit
+
+	// compiles our circuit into a R1CS
+	r1cs, err := frontend.Compile(ecc.BN254, backend.GROTH16, &cubicCircuit)
+	assert.NoError(err)
+
+	{
+		var witness Circuit
+		witness.X.Assign(42)
+		witness.Y.Assign(42)
+
+		assert.ProverFailed(r1cs, &witness)
+	}
+
+	{
+		var witness Circuit
+		witness.X.Assign(3)
+		witness.Y.Assign(35)
+		assert.ProverSucceeded(r1cs, &witness)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module example.com/gnarkcubic
+
+go 1.16
+
+require (
+	github.com/consensys/gnark v0.4.0
+	github.com/consensys/gnark-crypto v0.4.1-0.20210428083642-6bd055b79906
+)
